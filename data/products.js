82d212194f16a24b66cf9522679bdfb23de181ad
Soupyh <bleal2020@gmail.com>
2025-09-23 14:36:38 -0300

    Logos y modificacion del productos

--- data/products.js
+++ data/products.js
@@ -1,9 +1,11 @@
 // data/products.js
 window.products = [
-  { id: "p001", name: "Control Pro Gamer", price: 29990, image: "./assets/IMG/LogoRbGames.png" },
-  { id: "p002", name: "Teclado Mecánico RGB", price: 45990, image: "./assets/IMG/LogoRbGames.png" },
-  { id: "p003", name: "Audífonos Inalámbricos", price: 39990, image: "./assets/IMG/LogoRbGames.png" },
-  { id: "p004", name: "Mouse Gamer 7200DPI", price: 19990, image: "./assets/IMG/LogoRbGames.png" },
-  { id: "p005", name: "Silla Ergonómica RGB", price: 129990, image: "./assets/IMG/LogoRbGames.png" },
-  { id: "p006", name: "Monitor 24'' 144Hz", price: 189990, image: "./assets/IMG/LogoRbGames.png" }
+  { id: "p001", name: "Grand Theft Auto V", price: 29990, image: "./assets/IMG/Gtav.png" },
+  { id: "p002", name: "Left 4 Dead 2", price: 45990, image: "./assets/IMG/lf4d.png" },
+  { id: "p003", name: "Destiny 2", price: 39990, image: "./assets/IMG/destiny2.png" },
+  { id: "p004", name: "Delta Force", price: 19990, image: "./assets/IMG/deltaforce.png" },
+  { id: "p005", name: "Garry's Mod", price: 19990, image: "./assets/IMG/garrysmod.webp" },
+  { id: "p006", name: "Payday 2", price: 45990, image: "./assets/IMG/payday.webp" },
+  { id: "p007", name: "Team Fortress 2", price: 11990, image: "./assets/IMG/teamfortress.png" },
+  { id: "p008", name: "EA SPORTS FC™ 25", price: 12990, image: "./assets/IMG/fc25.webp" }
 ];
